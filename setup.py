58a7ba7804c7f3230d8eff59f0e4e42c3217c7a3
yucetekol <yucetekol@0a9920a7-e519-0410-b624-a5adc94349d4>
2007-05-29 11:17:01 +0000

    updated setup.py
    git-svn-id: http://pyswip.googlecode.com/svn/trunk@20 0a9920a7-e519-0410-b624-a5adc94349d4

--- setup.py
+++ setup.py
@@ -7,24 +7,26 @@ import os.path
 from distutils.core import setup
 
 setup(name="pyswip",
-		version="0.1.1",
+		version="0.1.2",
 		url="http://code.google.com/p/pyswip/",
 		download_url="http://code.google.com/p/pyswip/downloads/list",
 		author="Yuce Tekol",
 		author_email="yucetekol@gmail.com",
 		description="PySWIP enables querying SWI-Prolog in your Python programs.",
-		long_description="""PySWIP is a GPL'd Python - SWI-Prolog bridge enabling to query SWI-Prolog in your Python programs.
+		long_description="""PySWIP is a GPL'd Python - SWI-Prolog bridge which enables querying SWI-Prolog in your Python programs.
+		
+		PySWIP includes both an (incomplete) SWI-Prolog foreign language interface and a utity class that makes it easy querying SWI-Python.
 		
 		Example:
-			>>> from pyswip.util import PrologRunner
-			>>> prolog = PrologRunner()
-			>>> prolog.query("assertz(father(michael,john)).")
+			>>> from pyswip.util import Prolog
+			>>> prolog = Prolog()
+			>>> prolog.assertz("father(michael,john)")
 			[{}]
-			>>> prolog.query("assertz(father(michael,gina)).")
+			>>> prolog.assertz("father(michael,gina)")
 			[{}]
-			>>> prolog.query("father(michael,X).")
+			>>> list(prolog.query("father(michael,X)"))
 			[{'X': 'john'}, {'X': 'gina'}]
-			>>> for soln in prolog.queryGenerator("father(X,Y)."):
+			>>> for soln in prolog.query("father(X,Y)"):
 			...     print soln["X"], "is the father of", soln["Y"]
 			...
 			michael is the father of john
