efb1623e30d980871ec9b6f5f744aee812cef41e
rotter.manuel@gmail.com <rotter.manuel@gmail.com@0a9920a7-e519-0410-b624-a5adc94349d4>
2011-03-14 15:17:29 +0000

    only try to get it from swipl executable; safer because lib version will always fit executable version; dropping support for older versions
    git-svn-id: http://pyswip.googlecode.com/svn/trunk@109 0a9920a7-e519-0410-b624-a5adc94349d4

--- pyswip/load_library.py
+++ pyswip/load_library.py
@@ -4,52 +4,30 @@ import sys
 
 def loadLibrary():
     '''tries to load the swi-prolog shared library'''
-    for libname in ('swipl', 'pl'):
-        # try to use find_library from ctypes
-        print('try find_library()...')
-        path = find_library(libname)
-        if path:
-            try:
-                CDLL(path) # TODO: just check whether it's loadable?
-                break
-            except OSError:
-                pass
-        # try to simply open the lib
-        print('try CDLL()...')
-        try:
-            if sys.platform[:3] in ('win', 'cyg'):
-                try:
-                    path = '%s.dll' % libname
-                    CDLL(path)
-                    break
-                except:
-                    path = 'lib%s.dll' % libname
-            elif sys.platform[:3] in ('dar', 'os2'):
-                path = 'lib%s.dylib' % libname
-            else: # assume UNIX-like
-                path = 'lib%s.so' % libname
-            CDLL(path)
-            break
-        except OSError:
-            # try current working directory
-            try:
-                print('try cwd...')
-                path = './' + path
-                CDLL(path)
-                break
-            except OSError:
-                pass
-    else:
-        # try to get lib path from the swipl executable
-        # TODO: check whether this is reliable(different versions)
-        # does not work for windows currently
-        print('try swipl executable...')
-        from subprocess import check_output
-        
-        try:
+    # try to get lib path from the swipl executable
+    # TODO: check whether this is reliable(different versions)
+    from subprocess import Popen, PIPE
+    try:
+        if sys.platform[:3] in ('win', 'cyg'):
             # probably raises OSError:
-            ret = check_output(['swipl', '--dump-runtime-variables'])
-            ret = ret.replace('\n', '').split(';') # use .decode() in python3
+            cmd = Popen(['swipl', '--dump-runtime-variables'], stdout=PIPE)
+            ret = cmd.communicate()
+            
+            ret = ret[0].replace('\r\n', '').split(';') # use .decode() in python3
+            if ret[9] == 'PLSHARED="yes"':
+                print('shared true')
+                path = (ret[1].split('=',1)[1] + '/bin/' +
+                        ret[4].split('=',1)[1][4:][:5] + '.' +
+                        ret[7].split('=',1)[1]).replace('"', '')
+                print(path)
+            else:
+                raise OSError
+        else: # assume UNIX-like
+            # probably raises OSError:
+            cmd = Popen(['swipl', '--dump-runtime-variables'], stdout=PIPE)
+            ret = cmd.communicate()
+            
+            ret = ret[0].replace('\n', '').split(';') # use .decode() in python3
             
             if ret[9] == 'PLSHARED="yes"':
                 print('shared true')
@@ -59,8 +37,8 @@ def loadLibrary():
                         ret[7].split('=',1)[1]).replace('"', '')
             else:
                 raise OSError
-        except OSError:
-            raise ImportError('Could not find library "libswipl" or "libpl"')
+    except OSError:
+        raise ImportError('Could not find library "libswipl" or "libpl"')
     print('return')
     return CDLL(path)
 
