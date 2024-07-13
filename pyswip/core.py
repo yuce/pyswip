4e8dc5c89db5852449bd2c1c856c0635d24bf420
Guglielmo Gemignani <2789215+g-gemignani@users.noreply.github.com>
2024-07-13 22:59:15 +0200

    Add abort functionality and handle exception in cleanup (#150)
    * Try-except cleanup Prolog
    * Feature: Add abort method to Prolog class

--- pyswip/core.py
+++ pyswip/core.py
@@ -1367,5 +1367,9 @@ def cleanupProlog():
         # TODO Prolog documentation says cleanup with code 0 may be interrupted
         # If the program has come to an end the prolog system should not
         # interfere with that. Therefore we may want to use 1 instead of 0.
-        PL_cleanup(int(_hook.exit_code or 0))
+        try:
+            exit_code = int(_hook.exit_code or 0)
+        except ValueError:
+            exit_code = 0
+        PL_cleanup(exit_code)
         _isCleaned = True
